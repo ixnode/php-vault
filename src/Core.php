61a9c60b8b12181e2021ba4c6241593391c6051c
Björn Hempel <bjoern@hempel.li>
2021-04-18 12:40:42 +0200

    Start with version v1.0.0

--- src/Core.php
+++ src/Core.php
@@ -34,7 +34,7 @@ class Core
 {
     const NAME = 'PHPVault';
 
-    const VERSION = '1.0.0';
+    const VERSION = 'v1.0.0';
 
     const CORE_PUBLIC_KEY = 'zodfptod/VHjzzgdPgT2vk7RThdowsYXLiPP+eNI6D4=';
 
